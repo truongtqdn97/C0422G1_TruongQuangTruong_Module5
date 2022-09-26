dcda75f3d4ee619520bf1ecb822a8ebcc6f5464e
truongtqdn97 <truong.dn97@gmail.com>
2022-09-26 15:06:46 +0700

    ss5 component interaction

--- furama/final/furama/src/app/app.module.ts
+++ furama/final/furama/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FacilityListComponent } from './facility-list/facility-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { FacilityCreateComponent } from './facility-create/facility-create.component';
+import { FacilityUpdateComponent } from './facility-update/facility-update.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import { CustomerUpdateComponent } from './customer-update/customer-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FacilityListComponent,
+    NavbarComponent,
+    FooterComponent,
+    FacilityCreateComponent,
+    FacilityUpdateComponent,
+    CustomerListComponent,
+    CustomerCreateComponent,
+    CustomerUpdateComponent
   ],
   imports: [
     BrowserModule,
